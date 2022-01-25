e5a37bc4eb68aad771c84cb85c8adf1cf74bd4fa
JonnyGraichen <graichen.jonny@gmx.de>
2022-01-25 17:24:25 +0100

    finallll

--- src/pages/Page3.jsx
+++ src/pages/Page3.jsx
@@ -5,7 +5,7 @@ import {Page, TextView, Switch, contentView, ToggleButton} from 'tabris';
 export class Page3 extends Page {
  constructor(properties) {
  super();
- this.set({title: 'Checkbox', ...properties}).append( 
+ this.set({title: 'Switch/Toggle', ...properties}).append( 
    
     new Switch({top: 10, left: 20})
   .onSelect(() => console.log('Switch toggled'))
